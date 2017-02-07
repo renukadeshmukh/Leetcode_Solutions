a122848d262a515d1d3d5b5dee7073146014b9d4
Renuka Deshmukh <renud1988@gmail.com>
2017-02-06 17:38:33 -0800

    268. Missing Number

---
+++ 268_MissingNumber.py
@@ -0,0 +1,23 @@
+'''
+268. Missing Number
+
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+'''
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        actual_sum = sum(nums)
+        cnt = len(nums)
+        expected_sum =  (cnt * (cnt+1))/2
+        missing = expected_sum - actual_sum
+        
+        return missing
+        

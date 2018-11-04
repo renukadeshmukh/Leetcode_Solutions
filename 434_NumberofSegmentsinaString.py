34a1fc01b53574c8f50ba67b185cd7434f34db48
Renuka Deshmukh (redeshmu) <redeshmu@cisco.com>
2018-11-03 19:43:12 -0700

    434. Number of Segments in a String

---
+++ 434_NumberofSegmentsinaString.py
@@ -0,0 +1,24 @@
+'''
+434. Number of Segments in a String
+
+Count the number of segments in a string, where a segment is defined to be a 
+contiguous sequence of non-space characters.
+Please note that the string does not contain any non-printable characters.
+
+Example:
+Input: "Hello, my name is John"
+Output: 5
+'''
+
+'''
+RUNTIME COMPLEXITY: O(N)
+SPACE COMPLEXITY: O(1)
+'''
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.split())
+

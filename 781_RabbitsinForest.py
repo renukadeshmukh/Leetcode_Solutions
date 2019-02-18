1801d3c195d88ba9718b9bff16d33dffd624cc1b
Renuka Deshmukh (redeshmu) <redeshmu@cisco.com>
2019-02-18 12:58:25 -0800

    781. Rabbits in Forest

---
+++ 781_RabbitsinForest.py
@@ -0,0 +1,61 @@
+'''
+781. Rabbits in Forest
+
+In a forest, each rabbit has some color. Some subset of rabbits (possibly all of 
+them) tell you how many other rabbits have the same color as them. Those answers 
+are placed in an array. Return the minimum number of rabbits that could be in 
+the forest.
+
+Examples:
+Input: answers = [1, 1, 2]
+Output: 5
+Explanation:
+The two rabbits that answered "1" could both be the same color, say red.
+The rabbit than answered "2" can't be red or the answers would be inconsistent.
+Say the rabbit that answered "2" was blue. Then there should be 2 other blue 
+rabbits in the forest that didn't answer into the array.
+The smallest possible number of rabbits in the forest is therefore 5: 
+3 that answered plus 2 that didn't.
+
+Input: answers = [10, 10, 10]
+Output: 11
+
+Input: answers = []
+Output: 0
+
+Note:
+answers will have length at most 1000.
+Each answers[i] will be an integer in the range [0, 999].
+'''
+
+'''
+ALGORITHM:
+If x+1 rabbits have same color, then we get x+1 rabbits who all answer x.
+now n rabbits answer x.
+If n % (x + 1) == 0, we need n / (x + 1) groups of x + 1 rabbits.
+If n % (x + 1) != 0, we need n / (x + 1) + 1 groups of x + 1 rabbits.
+
+RUNTIME COMPLEXITY: O(N)
+SPACE COMPLEXITY: O(N)
+'''
+
+from math import ceil
+class Solution(object):
+    def numRabbits(self, answers):
+        """
+        :type answers: List[int]
+        :rtype: int
+        """
+        res = 0
+        dic = {}
+        for a in answers:
+            if a not in dic:
+                dic[a] = 0
+            dic[a] += 1
+        
+        
+        count=0
+        for key,val in dic.iteritems():
+            count+=math.ceil(val/(key+1.0))*(key+1)
+        
+        return int(count)

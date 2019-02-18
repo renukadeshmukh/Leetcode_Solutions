72f0b5b6527fd0637fe0ab88fa3311428e387b3d
Renuka Deshmukh (redeshmu) <redeshmu@cisco.com>
2019-02-17 18:59:04 -0800

    775. Global and Local Inversions

---
+++ 775_GlobalandLocalInversions.py
@@ -0,0 +1,50 @@
+'''
+775. Global and Local Inversions
+
+We have some permutation A of [0, 1, ..., N - 1], where N is the length of A. The 
+number of (global) inversions is the number of i < j with 0 <= i < j < N 
+and A[i] > A[j]. The number of local inversions is the number of i with 
+0 <= i < N and A[i] > A[i+1].
+
+Return true if and only if the number of global inversions is equal to the 
+number of local inversions.
+
+Example 1:
+Input: A = [1,0,2]
+Output: true
+Explanation: There is 1 global inversion, and 1 local inversion.
+
+Example 2:
+Input: A = [1,2,0]
+Output: false
+Explanation: There are 2 global inversions, and 1 local inversion.
+
+Note:
+A will be a permutation of [0, 1, ..., A.length - 1].
+A will have length in range [1, 5000].
+The time limit for this problem has been reduced.
+'''
+
+'''
+ALGORITHM:
+Local Inversions are part of Global Inversions when j == i + 1.
+Thus, whenever we find inversions that j != i + 1, we return false.
+However, that will cause time complexity = O(n^2).
+Local inversions are actually swaps between two adjacent elements only. So 
+whenever we detect |i - A[i]| > 1, we return false.
+
+RUNTIME COMPLEXITY: O(N)
+SPACE COMPLEXITY: O(1)
+'''
+
+class Solution(object):
+    def isIdealPermutation(self, A):
+        """
+        :type A: List[int]
+        :rtype: bool
+        """
+        for i in range(len(A)):
+            if abs(i-A[i]) > 1:
+                return False
+        return True
+        

004a7c62df8ae433bec225373a2571fdc410972b
Renuka Deshmukh (redeshmu) <redeshmu@cisco.com>
2019-03-28 18:33:53 -0700

    80. Remove Duplicates from Sorted Array II

---
+++ 080_RemoveDuplicatesfromSortedArrayII.py
@@ -0,0 +1,62 @@
+'''
+80. Remove Duplicates from Sorted Array II
+
+Given a sorted array nums, remove the duplicates in-place such that duplicates 
+appeared at most twice and return the new length. Do not allocate extra space 
+for another array, you must do this by modifying the input array in-place with 
+O(1) extra memory.
+
+Example 1:
+Given nums = [1,1,1,2,2,3],
+Your function should return length = 5, with the first five elements of nums 
+being 1, 1, 2, 2 and 3 respectively.
+It doesn't matter what you leave beyond the returned length.
+
+Example 2:
+Given nums = [0,0,1,1,1,1,2,3,3],
+Your function should return length = 7, with the first seven elements of nums 
+being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+It doesn't matter what values are set beyond the returned length.
+
+Clarification:
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to 
+the input array will be known to the caller as well.
+Internally you can think of this:
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+'''
+
+'''
+ALGORITHM:
+1. Maintain 2 indexes. One to traverse the array, other to keep track of array
+   with duplicates removed. 
+
+RUNTIME COMPLEXITY: O(N)
+SPACE COMPLEXITY: O(1)
+'''
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        j = 2
+        for i in range(2, len(nums)):
+            if nums[i] == nums[j-1] == nums[j-2]:
+                continue
+            nums[j] = nums[i]
+            j +=1 
+        return j
+                
+s = Solution()
+print(s.removeDuplicates([1,1,1,2,2,3]))
+print(s.removeDuplicates([0,0,1,1,1,1,2,3,3]))

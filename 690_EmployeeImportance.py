d452115e31e6c6313cac5a36e0710e8b61cce0af
Renuka Deshmukh <renud1988@gmail.com>
2019-06-12 17:42:25 -0700

    690. Employee Importance

---
+++ 690_EmployeeImportance.py
@@ -0,0 +1,75 @@
+'''
+690. Employee Importance
+
+You are given a data structure of employee information, which includes the 
+employee's unique id, his importance value and his direct subordinates' id.
+
+For example, employee 1 is the leader of employee 2, and employee 2 is the leader 
+of employee 3. They have importance value 15, 10 and 5, respectively. Then 
+employee 1 has a data structure like [1, 15, [2]], and employee 2 has 
+[2, 10, [3]], and employee 3 has [3, 5, []]. Note that although employee 3 is 
+also a subordinate of employee 1, the relationship is not direct.
+
+Now given the employee information of a company, and an employee id, you need to 
+return the total importance value of this employee and all his subordinates.
+
+Example 1:
+
+Input: [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
+Output: 11
+Explanation:
+Employee 1 has importance value 5, and he has two direct subordinates: employee 
+2 and employee 3. They both have importance value 3. So the total importance 
+value of employee 1 is 5 + 3 + 3 = 11.
+ 
+
+Note:
+
+One employee has at most one direct leader and may have several subordinates.
+The maximum number of employees won't exceed 2000.
+'''
+
+
+'''
+ALGORITHM:
+1. Save all employees in a hashmap with key = employee_id with easy access.
+2. Use a queue to track employee and all the subordinates. 
+3. While len(queue) > 0, pop element from 0th index.
+4. Add the importance of this employee to total. 
+5. Enqueue all the subordinates for this employee
+
+RUNTIME COMPLEXITY: O(N)
+SPACE COMPLEXITY: O(N)
+'''
+
+"""
+# Employee info
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+"""
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        emp_map = {}
+        for emp in employees:
+            emp_map[emp.id] = emp
+            
+        queue = []
+        queue.append(id)
+        total_importance = 0
+        while len(queue) > 0:
+            emp_id = queue.pop(0)
+            total_importance += emp_map[emp_id].importance
+            queue.extend(emp_map[emp_id].subordinates)
+        return total_importance

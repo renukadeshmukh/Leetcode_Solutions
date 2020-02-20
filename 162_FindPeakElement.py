8fcd8fd4fcec4e3fb5a70419667bdb4a5498dc88
Renuka Deshmukh <renud1988@gmail.com>
2020-02-19 18:28:21 -0800

    162. Find Peak Element

--- 162_FindPeakElement.py
+++ 162_FindPeakElement.py
@@ -46,4 +46,35 @@ class Solution(object):
         while i < len(nums)-1 and nums[i] < nums[i+1]:
             i += 1
         return i
+
+    '''
+    We use a modification of this simple Binary Search to our advantage. We start 
+    off by finding the middle element, midmid from the given numsnums array. If 
+    this element happens to be lying in a descending sequence of numbers. or a 
+    local falling slope(found by comparing nums[i]nums[i] to its right neighbour), 
+    it means that the peak will always lie towards the left of this element. 
+    Thus, we reduce the search space to the left of midmid(including itself) and 
+    perform the same process on left subarray.
+
+    If the middle element, midmid lies in an ascending sequence of numbers, or a 
+    rising slope(found by comparing nums[i]nums[i] to its right neighbour), it 
+    obviously implies that the peak lies towards the right of this element. 
+    Thus, we reduce the search space to the right of midmid and perform the same 
+    process on the right subarray.
+
+    In this way, we keep on reducing the search space till we eventually reach a 
+    state where only one element is remaining in the search space. This single 
+    element is the peak element.
+    '''    
+    def findPeakElementLogarithmic(self, nums) :
+        l = 0
+        r = len(nums) - 1;
+        while l < r :
+            int mid = (l + r) / 2;
+            if nums[mid] > nums[mid + 1]:
+                r = mid;
+            else
+                l = mid + 1;
+        return l;
+    
         

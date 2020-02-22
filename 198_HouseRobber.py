29bc7f80141b42d9c183d3fe8fbd254305436fc6
Renuka Deshmukh <renud1988@gmail.com>
2020-02-22 13:00:44 -0800

    198. House Robber

--- 198_HouseRobber.py
+++ 198_HouseRobber.py
@@ -1,18 +1,61 @@
 '''
-
 198. House Robber
 
-You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, 
-the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and 
-it will automatically contact the police if two adjacent houses were broken into on the same night.
+You are a professional robber planning to rob houses along a street. Each house 
+has a certain amount of money stashed, the only constraint stopping you from 
+robbing each of them is that adjacent houses have security system connected and 
+it will automatically contact the police if two adjacent houses were broken into 
+on the same night.
 
-Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of 
-money you can rob tonight without alerting the police.
+Given a list of non-negative integers representing the amount of money of each 
+house, determine the maximum amount of money you can rob tonight without 
+alerting the police.
+'''
 
+'''
+ALGORITHM:
+Hint:
+Think of the item value taking optimization with Dynamic programming.
+1. Solve by dynamic programming with the Optimal substructure (i.e., recurrence 
+relationship) as following:
+
+Let Optimal[i] denotes the highest value taking, considering from house_#0 to house_#i
+Optimal[ i ]
+= Optimization of ( Take house_#i , or Not to take house_#i )
+= Max( Optimal[ i - 2 ] + nums[i] , Optimal[ i - 1 ] )
+
+RUNTIME COMPLEXITY: O(N)
+SPACE COMPLEXITY: O(N)
 '''
 
 class Solution(object):
-    def rob(self, nums):
+    #BETTER SOLUTION
+    def rob(self, nums: List[int]) -> int:
+        
+        size = len(nums)
+        value_dp = [ 0 for _ in range(size) ]
+        
+        if size == 0:
+            # Empty list
+            return 0
+        
+        elif size == 1:
+            # Only one item
+            return nums[0]
+    
+        else:
+            # Initialization
+            value_dp[0] = nums[0]
+            value_dp[1] = max(nums[0], nums[1])
+
+            # General case
+            for i in range(2, size):
+                value_dp[i] = max(value_dp[i-2] + nums[i], value_dp[i-1] )
+
+            return value_dp[-1]
+
+    #OLD Solution
+    def rob2(self, nums):
         """
         :type nums: List[int]
         :rtype: int

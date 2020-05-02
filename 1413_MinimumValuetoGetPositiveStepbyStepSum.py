dad0464d37b28aef3498b3702ffa4859ea26103b
Renuka Deshmukh <renud1988@gmail.com>
2020-05-02 16:29:11 -0700

    1413. Min Value to Get Positive Step by Step Sum

---
+++ 1413_MinimumValuetoGetPositiveStepbyStepSum.py
@@ -0,0 +1,59 @@
+'''
+1413. Minimum Value to Get Positive Step by Step Sum
+
+Given an array of integers nums, you start with an initial positive value 
+startValue. In each iteration, you calculate the step by step sum of startValue 
+plus elements in nums (from left to right).
+Return the minimum positive value of startValue such that the step by step sum 
+is never less than 1.
+
+Example 1:
+Input: nums = [-3,2,-3,4,2]
+Output: 5
+Explanation: If you choose startValue = 4, in the third iteration your step by 
+step sum is less than 1.
+                step by step sum
+                startValue = 4 | startValue = 5 | nums
+                  (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+                  (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+                  (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+                  (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+                  (4 +2 ) = 6  | (5 +2 ) = 7    |   2
+
+Example 2:
+Input: nums = [1,2]
+Output: 1
+Explanation: Minimum start value should be positive. 
+
+Example 3:
+Input: nums = [1,-2,-3]
+Output: 5
+ 
+Constraints:
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100
+'''
+
+'''
+ALGORITHM:
+1. Find result of each step sum and keep track of minimum sum found so far 
+2. the startValue to keep the step sum always positive will be abs(min_sm) + 1
+
+RUNTIME COMPLEXITY: O(N)
+SPACE COMPLEXITY: O(1) 
+'''
+
+class Solution(object):
+    def minStartValue(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        min_step_sm = 0
+        sm = 0
+        for n in nums:
+            sm += n
+            min_step_sm = min(min_step_sm, sm)
+        
+        
+        return abs(min_step_sm) + 1
